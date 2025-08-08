8612b949fcd579983a12926a4ec7a3900e0b9b06
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-08 19:24:06 +0530

    lazy initialization

---
+++ 06-lazy-initialization/src/main/java/com/spring/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,19 @@
+package com.spring.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+
+@Lazy
+public class CricketCoach implements Coach{
+    
+    public CricketCoach(){
+        System.out.println("In Constructor: "+ getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout(){
+        return "Practice bowling daily for 20 minutes 🎯";
+    }
+}
---
+++ 06-lazy-initialization/src/main/java/com/spring/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.spring.springcoredemo.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.springcoredemo.common.Coach;
+
+@RestController
+public class DemoController {
+    public Coach myCoach;
+    
+    @Autowired
+    public DemoController(@Qualifier("trackCoach") Coach theCoach){
+        System.out.println("In Constructor: "+ getClass().getSimpleName());
+        myCoach = theCoach;
+
+    }
+    
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
